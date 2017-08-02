aae4d308f453cf5eb3384ee4de2e0aae4bdc7e19
cuiyu4345 <646017778@qq.com>
2017-08-02 22:49:38 +0800

    尾部插入节点有头结点

--- exam0802/linklist.cpp
+++ exam0802/linklist.cpp
@@ -22,28 +22,34 @@ struct node * creat_node(int data)
 	return p;
 }
 
-//尾部插入节点（无空节点）
+//尾部插入节点（有空节点）
 void insert_tail(struct node *ph, struct node *neww)
 {
 	struct node *p = ph;
+	int cnt = 0;
 
 	while (NULL != p->pNext)
 	{
 		p = p->pNext;
+		cnt ++;
 	}
 	p->pNext = neww;
+
+	ph->data = cnt + 1;
 
 }
 
 int main(void)
 {
-	struct node *pHeader = creat_node(1);
+	struct node *pHeader = creat_node(0);
 	insert_tail(pHeader, creat_node(22));
 	insert_tail(pHeader, creat_node(32));
+	insert_tail(pHeader, creat_node(20));
 
-	printf("node1 data: %d.\n", pHeader->data);
-	printf("node2 data: %d.\n", pHeader->pNext->data);
-	printf("node3 data: %d.\n", pHeader->pNext->pNext->data);
+	printf("Header node data: %d.\n", pHeader->data);
+	printf("node1 data: %d.\n", pHeader->pNext->data);
+	printf("node2 data: %d.\n", pHeader->pNext->pNext->data);
+	printf("node3 data: %d.\n", pHeader->pNext->pNext->pNext->data);
 
 
 
