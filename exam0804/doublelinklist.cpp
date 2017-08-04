6a2218c107b3d71578d420b0323c5e0669dbfab3
cuiyu4345 <646017778@qq.com>
2017-08-04 16:44:54 +0800

    双链表前向插入提交

--- exam0804/doublelinklist.cpp
+++ exam0804/doublelinklist.cpp
@@ -38,13 +38,36 @@ void insert_tail(struct node *ph, struct node *neww)
 	p->pNext = neww;
 	neww->pPrev = p;
 }
+
+//新节点neww前插入链表ph中
+void insert_head(struct node *ph, struct node *neww)
+{
+	
+	neww->pNext = ph->pNext;                 //新节点next指针指向原来第一个有效节点地址
+
+	if (ph->pNext != NULL)
+	{
+		ph->pNext->pPrev = neww;             //原来第一个有效节点pPrev指向新节点地址
+
+	}
+	                 
+	ph->pNext = neww;                        //头节点next指针指向新节点地址
+	neww->pPrev = ph;                        //新节点pPrev指向头节点 
+
+	
+}
 int main(void)
 {
 	struct node *pHeader = create_node(0);       //头指针
 
-	insert_tail(pHeader, create_node(11));
-	insert_tail(pHeader, create_node(12));
-	insert_tail(pHeader, create_node(13));
+	//insert_tail(pHeader, create_node(11));
+	//insert_tail(pHeader, create_node(12));
+	//insert_tail(pHeader, create_node(13));
+
+	insert_head(pHeader, create_node(11));
+	insert_head(pHeader, create_node(12));
+	insert_head(pHeader, create_node(13));
+
 
 	//遍历
 	printf("node 1 data: %d.\n", pHeader->pNext->data);
