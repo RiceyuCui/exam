c229bd418348cea3a7bbcb2dd4f98a03749024c4
cuiyu4345 <646017778@qq.com>
2017-08-14 22:18:06 +0800

    tinygame commit

--- exa0814/tsin.cpp
+++ exa0814/tsin.cpp
@@ -1,37 +1,72 @@
 #include <iostream>
-#include <cmath>
+#include <cstdlib>
 using namespace std;
 
-const double TINY_VALUE = 1e-10;
+//投骰子、计算和数、输出和数
 
-double tsin(double x)
+int rollDice()
 {
-	double g = 0;
-	double t = x;
-	int n = 1;
-	do
-	{
-		g += t;
-		n++;
-		t = -t * x * x / (2 * n -1) / (2 * n -2);
-	}while (fabs(t) >= TINY_VALUE);
+	int die1 = 1 + rand() % 6;
+	int die2 = 1 + rand() % 6;
+	int sum = die1 + die2;
 
-	return g;
+	cout << "player rolled " << die1 << "+" << die2 << "=" << sum << endl;
+
+	return sum;
 }
 
+enum GameState{WIN, LOSE, PLAYING};
 
-int main(void)
+int main()
 {
-	double k, r, s;
-	cout << "r  = ";
-	cin >> r;
-	cout << "s = " ;
-	cin >> s;
-	if (r * r <= s * s)
-		k = sqrt(tsin(r)*tsin(r)+tsin(s)*tsin(s));
-	else
-		k = tsin(r * s) / 2;
-	cout << k << endl;
-	return 0;
+	int sum, myPoint;
+	GameState status;
+	unsigned seed;
+	int rollDice();
+	cout << "Please enter an unsigned integer: ";
+	cin >> seed;              //输入随机数种子
+	srand(seed);              //将种子传递给rand()
+	sum = rollDice();         //第一轮投骰子、计算和数
+	
+	switch(sum)
+	{
+	case 7:                   //如果和数为7或11则为胜，状态为WIN
+	case 11:
+		status = WIN;
+		break;
+	case 2:                   //和数为2、3或12则为负，状态为LOSE
+	case 3:
+	case 12:
+		status = LOSE;
+		break;
+	default:                   //其他情况，尚无结果，状态为PLAYING，记下点数
+		status = PLAYING;
+		 myPoint = sum;
+		 cout << "point is " << myPoint <<endl;
+		 break;
+	}
 
+	while (status == PLAYING)
+	{
+		sum = rollDice();
+		if (sum == myPoint)
+		{
+			status = WIN;
+		}
+		else if (sum == 7)
+			status = LOSE;
+	}
+
+	if (sum == WIN)
+	{
+		cout << "player wins" << endl;
+
+	}
+	else 
+	{
+		cout << "player loses" << endl;
+	}
+
+	
+	return 0;
 }
